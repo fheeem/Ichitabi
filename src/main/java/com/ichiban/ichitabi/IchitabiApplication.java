c7e6e496e5aff018cab6b272c523d78fa9ae5a02
devbhkim0707 <devbhkim0707@gmail.com>
2025-12-30 18:06:05 +0900

    Initial commit

---
+++ src/main/java/com/ichiban/ichitabi/IchitabiApplication.java
@@ -0,0 +1,13 @@
+package com.ichiban.ichitabi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IchitabiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IchitabiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ichiban/ichitabi/IchitabiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ichiban.ichitabi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IchitabiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
